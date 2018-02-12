f3e31b8f8cbf2379e454cffb706af83a3ff028fc
ymnia <nielshubregtse@hotmail.com>
2018-02-12 13:36:54 +0100

    feebl notif

--- GitOut/Services/FeatureBranch.cs
+++ GitOut/Services/FeatureBranch.cs
@@ -1,4 +1,5 @@
-﻿using FluentScheduler;
+﻿using System.Diagnostics;
+using FluentScheduler;
 using GitOut.Utility;
 using System.IO;
 using System.Linq;
@@ -7,9 +8,10 @@ namespace GitOut.Services
 {
   public class FeatureBranch : Registry
   {
+    private static string FeboHost = @"http://feebl.azurewebsites.net/Service.asmx";
     public FeatureBranch()
     {
-      Schedule(Process).NonReentrant().ToRunNow().AndEvery(5).Minutes();
+      Schedule(Process).NonReentrant().ToRunNow().AndEvery(10).Minutes();
     }
 
     public void Process()
@@ -19,11 +21,12 @@ namespace GitOut.Services
       // in the future, i would like guidelines to define branch names, so this can be done automatically based on the branch name
       // for instance, FOO should always be pulled into any FOO#BAR branch unconditionally
 
-      File.ReadAllLines("Services/FeatureBranch.data")
+      var lines = File.ReadAllLines("Services/FeatureBranch.data")
           .Where(x => !string.IsNullOrWhiteSpace(x) && !x.StartsWith("//"))
           .Select(x => x.Trim().Split(';'))
-          .ToList()
-          .ForEach(c => GitHub.Post($"repos/{GitHub.Main}/{c[0]}/pulls", $"{{\"title\": \"{c[1]} → {c[2]}\", \"body\": \"Automatic PR for feature branch\", \"head\": \"{c[1]}\", \"base\": \"{c[2]}\"}}", out var _));
+          .ToList();
+          lines.ForEach(c => GitHub.Post($"repos/{GitHub.Main}/{c[0]}/pulls", $"{{\"title\": \"{c[1]} → {c[2]}\", \"body\": \"Automatic PR for feature branch\", \"head\": \"{c[1]}\", \"base\": \"{c[2]}\"}}", out var _));
+      Feebl.Client.Service.Update(out var _1, FeboHost, "GitOut", "Diract", "AutoPull, branches", lines.Count);
     }
   }
 }
